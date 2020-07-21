dbb42dfaf9ab502f3cd4989b036d19b9e3772294
JamesLee <48775473+titanian229@users.noreply.github.com>
2020-07-21 16:22:04 -0400

    Updates

---
+++ precache-manifest.61dc88f2da9bde28d1e02909cc56d5c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883673bb5e2e6180d3cd02118ac33b81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b0cdf6774b97d4035ed",
+    "url": "/static/css/2.4d550f1d.chunk.css"
+  },
+  {
+    "revision": "e30257c710cd7cfa73af",
+    "url": "/static/css/main.9da8596c.chunk.css"
+  },
+  {
+    "revision": "0b0cdf6774b97d4035ed",
+    "url": "/static/js/2.93e80ee9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.93e80ee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e30257c710cd7cfa73af",
+    "url": "/static/js/main.9d7e065f.chunk.js"
+  },
+  {
+    "revision": "4766f1b12db264376854",
+    "url": "/static/js/runtime-main.9d3e701f.js"
+  }
+]);
